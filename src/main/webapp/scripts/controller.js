b999bfded9b03ae18b024f6a10b9d78a21f1004c
nitink <nitink@nitink-HP-Notebook>
2016-03-24 16:23:05 +0530

    Cleaning up and starting a new gradle project.

--- src/main/webapp/scripts/controller.js
+++
@@ -1,9 +0,0 @@
-var sort = {
-	input : "12,13",
-	output : ""
-};
-
-var appname = angular.module('algorithms', []);
-appname.controller('sortingController', [ '$scope', function($scope) {
-	$scope.sort = sort;
-} ]);
